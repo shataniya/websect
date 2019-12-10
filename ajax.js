ce7621f4ef66401efbb5310148f952974dbe1a61
shataniya <47215073+shataniya@users.noreply.github.com>
2019-12-10 18:11:40 +0800

    Add files via upload

--- ajax.js
+++ ajax.js
@@ -1,25 +1,113 @@
+const zlib = require("zlib")
+const Buffer = require("buffer").Buffer
+const https = require("https")
+const http = require("http")
+const url = require("url")
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
-const ajax = (function(){
+
+function formate(data){
+    if(typeof data === "string"){
+        return data
+    }
+    var str = ""
+    for(let o in data){
+        str = str + o + "=" + data[o] + "&"
+    }
+    return str.replace(/&$/g,"")
+}
+
+var ajax = (function(){
     var xhr = new XMLHttpRequest()
     return {
-        get:function(url){
-            var URL = encodeURI(url)
+        get:function(path,flag){
+            var URL = {}
+            if(typeof path === "object"){
+                var opt = url.parse(path.url)
+                var protocol = opt.protocol
+                URL.hostname = opt.hostname
+                URL.path = opt.path
+                URL.headers = path.headers
+            }else{
+                URL = path
+                var protocol = url.parse(path).protocol
+            }
+            if(typeof path === "string" && typeof flag === "boolean" && flag){
+                URL = encodeURI(path)
+            }
             return new Promise((resolve,reject)=>{
-                xhr.open("GET",URL,true)
-                xhr.send(null)
-                xhr.onreadystatechange = function(){
-                    if(xhr.readyState === 4){
-                        try{
-                            resolve(JSON.parse(xhr.responseText))
-                        }catch(err){
-                            resolve(xhr.responseText)
-                        }
-                    }
+                if(protocol === "http:"){
+                    http.get(URL,(res)=>{
+                        var text = []
+                        var restext = null
+                        res.on("data",function(chunk){
+                            text.push(chunk)
+                        })
+                        res.on("end",function(){
+                            restext = Buffer.concat(text)
+                            var response = {}
+                            response.buffer = restext
+                            response.text = restext.toString()
+                            response.headers = res.headers
+                            response.rawHeaders = res.rawHeaders
+                            response.uncompress = function(compress_method){
+                                var cm = compress_method || this.headers["content-encoding"]
+                                if(cm === "deflate"){
+                                    try{
+                                        var uncompressed = zlib.inflateSync(this.buffer)
+                                    }catch(err){
+                                        var uncompressed = zlib.inflateRawSync(this.buffer)
+                                    }
+                                    return uncompressed
+                                }
+                                if(cm === "gzip"){
+                                    var uncompressed = zlib.unzipSync(this.buffer)
+                                    return uncompressed
+                                }
+                            }
+                            resolve(response)
+                        })
+                    }).end()
+                }
+                if(protocol === "https:"){
+                    https.get(URL,(res)=>{
+                        var text = []
+                        var restext = null
+                        res.on("data",function(chunk){
+                            text.push(chunk)
+                        })
+                        res.on("end",function(){
+                            restext = Buffer.concat(text)
+                            var response = {}
+                            response.buffer = restext
+                            response.text = restext.toString()
+                            response.headers = res.headers
+                            response.rawHeaders = res.rawHeaders
+                            response.uncompress = function(compress_method){
+                                var cm = compress_method || this.headers["content-encoding"]
+                                if(cm === "deflate"){
+                                    try{
+                                        var uncompressed = zlib.inflateSync(this.buffer)
+                                    }catch(err){
+                                        var uncompressed = zlib.inflateRawSync(this.buffer)
+                                    }
+                                    return uncompressed
+                                }
+                                if(cm === "gzip"){
+                                    var uncompressed = zlib.unzipSync(this.buffer)
+                                    return uncompressed
+                                }
+                            }
+                            resolve(response)
+                        })
+                    }).end()
                 }
             })
         },
-        post:function(url,data){
-            var URL = encodeURI(url)
+        post:function(path,data,flag){
+            var URL = path
+            if(typeof flag === "boolean" && flag){
+                URL = encodeURI(path)
+            }
             return new Promise((resolve,reject)=>{
                 xhr.open("POST",URL,true)
                 xhr.send(formate(data))
@@ -36,16 +124,5 @@ const ajax = (function(){
         }
     }
 })();
-
-function formate(data){
-    if(typeof data === "string"){
-        return data
-    }
-    var str = ""
-    for(let o in data){
-        str = str + o + "=" + data[o] + "&"
-    }
-    return str.replace(/&$/g,"")
-}
 
 module.exports = ajax
